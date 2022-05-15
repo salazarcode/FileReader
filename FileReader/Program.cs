0e81a675c75f8bc319bdd7e33278d55690727c5c
Andrés <andresalteclado@hotmail.com>
2022-05-14 20:38:17 -0500

    Agregar archivos de proyecto.

---
+++ FileReader/Program.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string[]> results = new List<string[]>();
+
+            using (var reader = new StreamReader(@"C:\Users\andre\OneDrive\Desktop\example.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    string[] values = line.Split(',');
+
+                    results.Add(values);
+                }
+            }
+
+            Console.WriteLine(JsonConvert.SerializeObject(results));
+        }
+    }
+}
